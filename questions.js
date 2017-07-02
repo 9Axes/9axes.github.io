e655c84836480088c0dafb72ee7a9fca4c1c0860
Thor Keller <thorboomafoo@gmail.com>
2017-07-02 19:35:41 -0400

    initial commit

---
+++ questions.js
@@ -0,0 +1,272 @@
+questions = 
+    [[//Federal-Unitary`
+        [//4
+            "There is no need for a national government",
+            "Subdivisions of nations should have their own militaries"
+        ],
+        [//3
+            "State and local laws should have precedence over national laws",
+            "The national government can't understand what the people need.",
+            "People should hold more loyalty to their local state than their country."
+        ],
+        [//2
+            "Local governments should have more power than they currently do",
+            "Laws should vary from area to area and culture to culture within a country."
+        ],
+        [//1
+            "Local governments can understand their citizens better than the national government could",
+            "Local governments address issues that the national government would never touch without them"
+        ],
+        [//-1
+            "A country cannot function without a national government",
+            "The national government protects minorities better than some local governments do."
+        ],
+        [//-2
+            "The national government needs more power",
+            "Subdivisions should not be able to exempt from national laws"
+        ],
+        [//-3
+            "A group of states without a strong national government is merely an alliance, not a nation"
+        ],
+        [//-4
+            "There is no need for local or state governments",
+            "Laws should be completely consistent within all regions of a nation"
+        ]],[//Democratic-Authoritarian`
+        [//4
+            "Direct democracy is the best form of government",
+            "People should vote issue by issue themselves"
+        ],
+        [//3
+            "It is important that the government follows the majority opinion, even if it is wrong."
+        ],
+        [//2
+            "Elections are an effective way of showing government approval",
+            "The more people asked, the better chance there is of reaching the best decision."
+        ],
+        [//1
+            "Democracy is more than a decision-making process."
+        ],
+        [//-1
+            "The stronger the leadership, the better."
+        ],
+        [//-2
+             "A hierarchical state is best.",
+             "It is important that we further my group's goals above all others."
+             "Democracy makes it too easy for a majority too further itself at the expense of minorities"
+        ],
+        [//-3
+            "The general populace makes poor decisions.",
+            "Public opinion swings too much with emotional events to be useful for policy making"
+        ],
+        [//-4
+            "Elections are a waste of resources",
+            "Catering to popular opinion is detremental to a nation"
+        ]],[//Globalist-Isolationist
+        [//4
+            "A united world government would be beneficial to mankind."
+        ],
+        [//3
+            "The United Nations needs more power"
+        ],
+        [//2
+            "I support regional unions, such as the European Union.",
+            "Governments should be accountable to the international community."
+        ],
+        [//1
+            "International trade is generally beneficial."
+        ],
+        [//-1
+            "It is important to maintain our national sovereignty."
+        ],
+        [//-2
+            "International aid is a waste of money.",
+            "Alliances can pose a threat to a nation's sovereignty."
+        ],
+        [//-3
+            "My nation should stay out of international affairs",
+            "The United Nations should be abolished."
+        ],
+        [//-4
+            "Diplomacy only hurts nations"
+        ]],[//Militarist-Pacifist
+        [//4
+            "War is needed to make an economy thrive",
+            "A constant state of war benefits a country"
+        ],
+        [//3
+            "There should be compulsory military service",
+            "Only those who served in the military should be able to gain power in government"
+        ],
+        [//2
+            "Military action by our nation is often necessary to protect it."
+        ],
+        [//1
+            "A government must maintain a strong military to defend itself"
+        ],
+        [//-1
+            "It is more important to retain peaceful relations than to further our strength."
+        ],
+        [//-2
+            "Even when protesting an authoritarian government, violence is not acceptable."
+        ],
+        [//-3
+            "Military spending is a waste of money."
+        ],
+        [//-4
+            "War is never justified",
+            "Nations should not maintain a military"
+        ]],[//Security-Freedom
+        [//4
+            "Those who disagree with the government should be removed from the country",
+            "The government should monitor all citizens to combat terrorism"
+        ],
+        [//3
+            "Police need more power and protection",
+            "It is very important to maintain law and order."
+        ],
+        [//2
+            "If you have nothing to hide, then you shouldn't care about the government having access to your communications",
+            "The sacrifice of some civil liberties is necessary to protect us from acts of terrorism.",
+            "Government surveillance is necessary in the modern world."
+        ],
+        [//1
+            "The government should have access to the emails of suspected terrorists"
+        ],
+        [//-1
+            "Marijuana should be legalized"
+        ],
+        [//-2
+            "Abortion should be legal in all cases",
+            "Police should be regulated more",
+            "No authority should be left unquestioned."
+        ],
+        [//-3
+            "Victimless crimes (such as drug use) should not be crimes at all.",
+            "Most drugs should be legalized",
+            "Whistleblowers should be strongly protected"
+        ],
+        [//-4
+            "The very existence of the state is a threat to our liberty.",
+            "There should be no police force"
+        ]],[//Equality-Markets`
+        [//4
+            "Communism, if implemented correctly, would be a good form of economics",
+            "From each according to his ability, to each according to his need"
+        ],
+        [//3
+            "Oppression by corporations is more of a concern than oppression by governments.",
+            "Taxes should be increased on the rich to provide for the poor.",
+            "Public utilities like roads and electricity should be publicly owned."
+        ],
+        [//2
+            "I support single-payer, universal healthcare.",
+            "The wealthy have far more than they need"
+        ],
+        [//1
+            "It is necessary for the government to intervene in the economy to protect consumers."
+        ],
+        [//-1
+            "People should have the right to leave their wealth to their descendents",
+            "Excessive government intervention is a threat to the economy."
+        ],
+        [//-2
+            "It is better to maintain a balanced budget than to ensure welfare for all citizens."
+        ],
+        [//-3
+            "The freer the market, the freer the people"
+        ],
+        [//-4
+            "The government should not regulate the economy at all",
+            "The government should not break up monopolies"
+        ]],[//Secular-Religious
+        [//4
+            "Religion should be banned",
+            "Faith is complete nonsense"
+        ],
+        [//3
+            "Religious clothing should be banned from public",
+            "The government should not fund any religious organizations in any way"
+        ],
+        [//2
+            "I support gay marriage",
+        ],
+        [//1
+            "Religion does not belong in government"
+        ],
+        [//-1
+            "I believe there is something after death"
+        ],
+        [//-2
+            "I believe in a higher power",
+            "Religious orginizations often provide better assistance to the poor than the government",
+            "Children should be educated in religious values."
+        ],
+        [//-3
+            "Religion usually provides the truth",
+            "My religious values should be spread as much as possible.",
+            "Sex outside marriage is immoral."
+        ],
+        [//-4
+            "Theocracy is a good system of government",
+            "The government should support and only recognize one religion"
+        ]],[//Progressive-Traditional
+        [//4
+            "If AI could rule more effectively than humans, then AI should rule",
+            "Genetic modification is a force for good, even on humans."
+        ],
+        [//3
+            "Automation overall is extremely good for society",
+            "Traditions are of no value on their own."
+        ],
+        [//2
+            "We should put effort into getting a person on mars by 2025"
+            
+        ],
+        [//1
+            "Human Caused climate change is currently one of the greatest threats to our way of life.",
+            "Environmental regulations are essential.",
+            "A better world will come from automation, science, and technology."
+        ],
+        [//-1
+            "Technology is negatively affecting modern society",
+            "To chase progress at all costs is dangerous."
+        ],
+        [//-2
+            "Genetic modification should be used rarely, if ever",
+            "Society was better many years ago than it is now.",
+            "It is important that we maintain the traditions of our past."
+        ],
+        [//-3
+            "Traditional medicines are often more effective than modern medicines",
+            "The voting age should be raised",
+            "Maintaining family values is essential."
+        ],
+        [//-4
+            "The Internet should be banned"
+        ]],[//Assimilationist-Multiculturalist
+        [//4
+            "Foreigners should never enter the country"
+        ],
+        [//3
+            "Only extremely talented immigrants, if that, should be permitted to enter the country",
+            "If we accept migrants at all, it is important that they assimilate into our culture."
+        ],
+        [//2
+            "In general, immigrants are bad for the country"
+        ],
+        [//1
+            "Illegal immigrants should be deported in most or all cases"
+        ],
+        [//-1
+            "No cultures are superior to others."
+        ],
+        [//-2
+            "We should be taking in more refugees"
+        ],
+        [//-3
+            "Governments should be as concerned about foreign citizens as they are about those within their borders."
+        ],
+        [//-4
+            "Anybody who wants to enter the country should be able to"
+        ]
+    ]];
